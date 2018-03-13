a212fd306928ea979b5dbaf962aeca40b9fce047
Hayden Roberts <hayden.roberts@my.jcu.edu.au>
2018-03-13 12:15:55 +1000

    Add name and second letter printing program.

--- oddName.py
+++ oddName.py
@@ -1,1 +1,7 @@
 """ Hayden Roberts """
+
+user_name = input("Your name: ")
+if len(user_name.strip()) == 0:
+    print("You have not entered a name")
+else:
+    print("{}".format(user_name[1:-1:2]))
